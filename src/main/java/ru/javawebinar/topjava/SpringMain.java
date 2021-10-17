cf1026a5cc3e20ed2846159ae2d9c518fd8ba826
Frazer-dl <k.genov.java@yandex.ru>
2021-10-17 09:13:29 +0300

    HW02 users sorting

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -5,7 +5,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.web.MealServlet;
 import ru.javawebinar.topjava.web.SecurityUtil;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
@@ -19,16 +18,28 @@ public class SpringMain {
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "Abc", "1", "password", Role.ADMIN));
-            adminUserController.create(new User(null, "Dca", "3", "password", Role.USER));
-            adminUserController.create(new User(null, "Abc", "4", "password", Role.USER));
-            adminUserController.create(new User(null, "Z", "2", "password", Role.USER));
-            System.out.println(adminUserController.getAll());
-//            System.out.println(adminUserController.getAll());
+
+            adminUserController.create(new User(null, "Вася", "1", "password", Role.ADMIN));
+            adminUserController.create(new User(null, "Вася", "2", "password", Role.USER));
+            adminUserController.create(new User(null, "Вася Григорьев", "6", "password", Role.USER));
+            adminUserController.create(new User(null, "Вася", "5", "password", Role.USER));
+            adminUserController.create(new User(null, "Вася", "4", "password", Role.USER));
+            adminUserController.create(new User(null, "Даша", "13", "password", Role.USER));
+            adminUserController.create(new User(null, "Герман", "14", "password", Role.USER));
+            adminUserController.create(new User(null, "Вася", "3", "password", Role.USER));
+            adminUserController.create(new User(null, "Вася Астафьев", "7", "password", Role.USER));
+            adminUserController.create(new User(null, "Вася", "8", "password", Role.USER));
+            adminUserController.create(new User(null, "Вася", "9", "password", Role.USER));
+            adminUserController.create(new User(null, "Алла", "12", "password", Role.ADMIN));
+            adminUserController.create(new User(null, "Вася", "10", "password", Role.USER));
+            adminUserController.create(new User(null, "Алла", "11", "password", Role.ADMIN));
+            adminUserController.create(new User(null, "Герман", "15", "password", Role.USER));
+            adminUserController.getAll().forEach(System.out::println);
 
             MealRestController mealRestController = appCtx.getBean(MealRestController.class);
             SecurityUtil.setAuthUserId(1);
             mealRestController.update(new Meal(LocalDateTime.now().withSecond(0).withNano(0), "description", 100), 1);
+            mealRestController.getAll().forEach(System.out::println);
 //            mealRestController.create(new Meal(LocalDateTime.now().withSecond(0).withNano(0), "description", 50), 2);
 //            mealRestController.create(new Meal(LocalDateTime.now().withSecond(0).withNano(0), "description", 50), 2);
 //            mealRestController.update(new Meal(LocalDateTime.now().withSecond(0).withNano(0), "description", 511), 2);
