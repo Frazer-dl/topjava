36512f66381241390b210e3aad8a7c49c48057f7
Frazer-dl <Kostolom_171>
2021-11-10 22:54:36 +0300

    HW05 final fixed

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -1,11 +1,9 @@
 package ru.javawebinar.topjava.service.datajpa;
 
-import org.hibernate.Hibernate;
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.MealTestData;
-import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
